af926c87003f3a5ad476a9e2df4eade06360c540
Ben Kavouras <kavourbm@mail.uc.edu>
2023-01-13 18:35:18 -0500

    final HW1 run

--- Modules/kavouras1.py
+++ Modules/kavouras1.py
@@ -13,7 +13,7 @@ def returnfloat(bruh):
     return x
 
 def get2inputs(one,two,prompt0,prompt1,prompt2):
-    q = input(prompt0+" ["+prompt1+"/"+prompt2+"]")
+    q = input(prompt0+" ["+prompt1+"/"+prompt2+"] ")
     checker = 0
     while checker == 0:
         if yeet.any(one == q):
@@ -24,10 +24,10 @@ def get2inputs(one,two,prompt0,prompt1,prompt2):
             return checker
         else:
             print("Your input wasn't understood")
-            q = input("Please try again. ["+prompt1+"/"+prompt2+"]")
+            q = input("Please try again. ["+prompt1+"/"+prompt2+"] ")
 
 def get3inputs(one,two,three,prompt0,prompt1,prompt2,prompt3):
-    q = input(prompt0+" ["+prompt1+"/"+prompt2+"/"+prompt3+"]")
+    q = input(prompt0+" ["+prompt1+"/"+prompt2+"/"+prompt3+"] ")
     checker = 0
     while checker == 0:
         if yeet.any(one == q):
@@ -41,4 +41,4 @@ def get3inputs(one,two,three,prompt0,prompt1,prompt2,prompt3):
             return checker
         else:
             print("Your input wasn't understood")
-            q = input("Please try again. ["+prompt1+"/"+prompt2+"/"+prompt3+"]")
+            q = input("Please try again. ["+prompt1+"/"+prompt2+"/"+prompt3+"] ")
