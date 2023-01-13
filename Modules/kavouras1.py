5d3b54c9c28783d6c342e2e223e31bdcb9c05fd4
Ben Kavouras <kavourbm@mail.uc.edu>
2023-01-13 14:57:07 -0500

    Updated assignment 1 and kavouras1 for finish

--- Modules/kavouras1.py
+++ Modules/kavouras1.py
@@ -6,5 +6,39 @@ def returnfloat(bruh):
     if bruh == "pi" or bruh == "np.pi":
         x = yeet.pi
     else:
-        x = float(bruh)
+        try:
+            x = float(bruh)
+        except:
+            x = bruh
     return x
+
+def get2inputs(one,two,prompt0,prompt1,prompt2):
+    q = input(prompt0+" ["+prompt1+"/"+prompt2+"]")
+    checker = 0
+    while checker == 0:
+        if yeet.any(one == q):
+            checker = 1
+            return checker
+        elif yeet.any(two == q):
+            checker = 2
+            return checker
+        else:
+            print("Your input wasn't understood")
+            q = input("Please try again. ["+prompt1+"/"+prompt2+"]")
+
+def get3inputs(one,two,three,prompt0,prompt1,prompt2,prompt3):
+    q = input(prompt0+" ["+prompt1+"/"+prompt2+"/"+prompt3+"]")
+    checker = 0
+    while checker == 0:
+        if yeet.any(one == q):
+            checker = 1
+            return checker
+        elif yeet.any(two == q):
+            checker = 2
+            return checker
+        elif yeet.any(three == q):
+            checker = 3
+            return checker
+        else:
+            print("Your input wasn't understood")
+            q = input("Please try again. ["+prompt1+"/"+prompt2+"/"+prompt3+"]")
