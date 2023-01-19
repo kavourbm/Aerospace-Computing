bb0281b318f12925fe0929e49c291fb4304ef3e6
kavourbm <kavourbm@mail.uc.edu>
2023-01-18 21:17:35 -0500

    added LUsolve

--- Modules/textbook.py
+++ Modules/textbook.py
@@ -74,3 +74,19 @@ def LUdecomp(a,tol=1.0e-9):
                 a[i,k+1:n]-lam*a[k,k+1:n]
                 a[i,k] = lam
     return a,seq
+
+def LUsolve(a,b,seq):
+    n = len(a)
+
+    # Rearrange constant vector; store it in [x]
+    x = b.copy()
+    for i in range(n):
+        x[i] = b[seq[i]]
+
+    # Solution
+    for k in range(1,n):
+        x[k] = x[k] - yeet.dot(a[k,0:k],x[0:k])
+    x[n-1] = x[n-1]/a[n-1,n-1]
+    for k in range(n-2,-1,-1):
+        x[k] = (x[k]-yeet.dot(a[,k+1:n]))/a[k,k]
+    return x
