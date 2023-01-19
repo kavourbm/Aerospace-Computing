c8cb0e5579944449b2b75d733983134efd32b529
Ben Kavouras <kavourbm@mail.uc.edu>
2023-01-18 21:11:50 -0500

    added LUdecomp to textbook

--- Modules/textbook.py
+++ Modules/textbook.py
@@ -23,7 +23,7 @@ def gaussPivot(a,b,tol=1.0e-12):
         p = yeet.argmax(yeet.abs(a[k:n,k])/s[k:n])+k
         if abs(a[p,k]) < tol:
             print('Matrix is singular. unable to pivot')
-            break
+            return 0
         if p != k:
             swapRows(b,k,p)
             swapRows(s,k,p)
@@ -37,10 +37,40 @@ def gaussPivot(a,b,tol=1.0e-12):
                 b[i] = b[i] - lam*b[k]
     
     if abs(a[n-1,n-1]) < tol:
-        error.err('Matrix is singular')
+        print('Matrix is singular. Unable to pivot')
+        return 0
 
     #Back substitution
     b[n-1] = b[n-1]/a[n-1,n-1]
     for k in range(n-2,-1,-1):
         b[k] = (b[k]-yeet.dot(a[k,k+1:n],b[k+1:n]))/a[k,k]
     return b
+
+def LUdecomp(a,tol=1.0e-9):
+    n = len(a)
+    seq = yeet.array(range(n))
+
+    # Set up scale factors
+    s = yeet.zeros((n))
+    for i in range(n):
+        s[i] = max(abs(a[i,:]))
+
+    for k in range(0,n-1):
+
+        # Row interchange, if needed
+        p = yeet.argmax(yeet.abs(a[k:n,k])/s[k:n])+k
+        if abs(a[p,k]) < tol:
+            print('Matrix is singular. Unable to pivot')
+            return 0
+        if p != k:
+            swapRows(s,k,p)
+            swapRows(a,k,p)
+            swapRows(seq,k,p)
+
+        #Elimination
+        for i in range(k+1,n):
+            if a[i,k] != 0.0:
+                lam = a[i,k]/a[k,k]
+                a[i,k+1:n]-lam*a[k,k+1:n]
+                a[i,k] = lam
+    return a,seq
